9b60cff530fb5127b377229fcf006f0739eca42f
samathi <samathisapumana@gmail.com>
2023-01-19 12:38:52 -0800

    cobstructors

---
+++ makingYourOwnClasses/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makingYourOwnClasses
+{
+    //solution explore -> rigt click -> add -> class
+    internal class Class1
+    {
+        // have data , methods, constructors
+
+        private string title;
+        private string description;
+        private int pages;
+        private int wordCount;
+
+        public Class1(string title, string description, int pages)
+        {
+            this.title = title;
+            this.description = description;
+            this.pages = pages;
+        }
+    }
+}
